ac4ebb867852155314b5c4369642d22eb8658814
mohan <mohan.talachutla@gmail.com>
2024-12-23 05:25:22 +0530

    refactor GlobalModal, Label, Container

--- src/components/base/Label.jsx
+++ src/components/base/Label.jsx
@@ -1,3 +1,12 @@
-import { Label } from 'flowbite-react';
+const Label = ({ htmlFor, value }) => {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className="block text-sm font-medium text-gray-700 text-start"
+    >
+      {value}
+    </label>
+  );
+};
 
 export default Label;
--- src/components/common/GlobalModal.jsx
+++ src/components/common/GlobalModal.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import _ from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
 import { MODAL_TYPES } from '../../constants';
 import { hideModal } from '../../reducers/modal';
@@ -6,34 +8,73 @@ import Modal from '../base/Modal';
 const GlobalModal = () => {
   const dispatch = useDispatch();
   const { modal } = useSelector((state) => state?.modal);
-  const { display = false, type = MODAL_TYPES.DEFAULT } = modal;
+  const {
+    display = false,
+    dismissible = true,
+    type = MODAL_TYPES.DEFAULT,
+    data,
+  } = modal;
+
+  const Component = getModalByType(type);
   return (
     <Modal
       id="global-modal"
+      dismissible={dismissible}
       show={display}
       onClose={() => dispatch(hideModal())}
       size="md"
     >
-      {getModalByType(modal)}
+      <Render Component={Component} data={data} />
     </Modal>
   );
 };
 
-const DefaultModal = ({ header, body, footer }) => {
-  <div>
-    {header && <Modal.Header>{header}</Modal.Header>}
-    {body && <Modal.Body>{body}</Modal.Body>}
-    {footer && <Modal.Footer>{footer}</Modal.Footer>}
-  </div>;
-};
-
-const getModalByType = (modal) => {
-  switch (modal.type) {
+const getModalByType = (type) => {
+  switch (type) {
     //add more modals
+
+    //default
     case MODAL_TYPES.DEFAULT:
     default:
-      return <DefaultModal {...modal} />;
+      return DefaultComponent;
   }
+};
+
+const Render = ({ Component, data }) => {
+  if (_.isString(data)) {
+    return (
+      <Modal.Body>
+        <Component> {data} </Component>
+      </Modal.Body>
+    );
+  }
+  if (_.isObject(data)) {
+    return (
+      <React.Fragment>
+        {Component?.Header && (
+          <Modal.Header>
+            <Component.Header {...data} />
+          </Modal.Header>
+        )}
+        {Component && (
+          <Modal.Body>
+            <Component {...data} />
+          </Modal.Body>
+        )}
+        {Component?.Footer && (
+          <Modal.Footer>
+            <Component.Footer {...data} />
+          </Modal.Footer>
+        )}
+      </React.Fragment>
+    );
+  }
+  console.error('GlobalModal: Invalid modal data');
+  return null;
+};
+
+const DefaultComponent = ({ children, ...props }) => {
+  return <pre {...props}>{children}</pre>;
 };
 
 export default GlobalModal;
